062b0b0d35f20bb6a6c6b4e4b9320eb1a13ceabe
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2023-07-25 14:47:55 -0400

    Update Statics.hx
    It's not worth the trouble trying to figure out ``useLocation`` and ``useHistory``; I would instead recommend just creating a custom bind in JS

--- src/react/router/Statics.hx
+++ src/react/router/Statics.hx
@@ -11,7 +11,4 @@ extern class Statics {
 	static public var hashHistory:HashHistory;
 	static public var browserHistory:BrowserHistory;
 	static public var withRouter:Dynamic->Dynamic;
-
-	static public function useLocation<Q>():Location<Q>; // Avaliable for versions 4.0.0 and above
-    static public function useHistory():HashHistory; // Avaliable for versions 4.0.0 and above
 }

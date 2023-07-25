bbff969906738e05a50742cf4dbd9cdeb8ef6159
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2023-07-25 05:03:47 -0400

    Usable for versions 4.0.0 and above

---
+++ src/react/router/BrowserRouter.hx
@@ -0,0 +1,12 @@
+package src.react.router;
+
+/*
+* Avaliable for versions 4.0.0 and above
+*/
+
+#if (jsImport)
+@:js.import('react-router-dom', 'BrowserRouter')
+#else
+@:jsRequire('react-router-dom', 'BrowserRouter')
+#end
+extern class BrowserRouter extends react.ReactComponent {}
--- src/react/router/Statics.hx
+++ src/react/router/Statics.hx
@@ -11,4 +11,7 @@ extern class Statics {
 	static public var hashHistory:HashHistory;
 	static public var browserHistory:BrowserHistory;
 	static public var withRouter:Dynamic->Dynamic;
+	
+	static public function useLocation<T>():T; // Avaliable for versions 4.0.0 and above
+	static public function useHistory<T>():T; // Avaliable for versions 4.0.0 and above
 }
